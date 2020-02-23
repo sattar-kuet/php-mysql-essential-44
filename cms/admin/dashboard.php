1f37f3c2b81ba0db5d134cbf13d34c90446f2fb2
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 15:14:52 +0600

    first commit

---
+++ cms/admin/dashboard.php
@@ -0,0 +1,23 @@
+<?php include('../include/admin_header.php');?>
+
+
+
+  <div class="wrapper">
+    <!-- Sidebar Holder -->
+    <?php include('partial/sidebar.php'); ?>
+
+    <!-- Page Content Holder -->
+    <div id="content">
+      <!-- top nav bar -->
+      <?php include('partial/top-nav.php');?>
+      <div class="container">
+               Dashboard
+      </div>
+
+    </div>
+  </div>
+
+<?php include('../include/footer.php');?>
+
+
+
---
+++ cms/admin/partial/sidebar.php
@@ -0,0 +1,17 @@
+  <?php $base_url = 'http://'.$_SERVER['SERVER_NAME'];?>
+  <nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>CMS</h3>
+        <strong>CMS</strong>
+    </div>
+
+    <ul class="list-unstyled components">
+        <li>
+            <a href="<?php echo $base_url;?>/cms/admin/new_post.php">
+                <i class="glyphicon glyphicon-pencil"></i>
+                New Post
+            </a>
+        </li>
+
+    </ul>
+</nav>
